0ed1129ea3ff0b97dca1515d2947a4a250268536
nekonekko <momoichigodayo+github@gmail.com>
2023-09-23 19:08:47 +0900

    chore: :children_crossing: change text to English

--- ui.py
+++ ui.py
@@ -5,37 +5,35 @@ from convert import copy_and_convert_all_excel
 sg.theme("DarkAmber")
 
 layout = [
-    [sg.Text("選択された変換元フォルダ下にあるすべてのxlsxファイルとxlsファイルをcsvに変換します。")],
-    [sg.Text("変換後のcsvファイルは、変換後フォルダ下にフォルダ構造を保った状態で入ります。")],
+    [sg.Text("Converts all xlsx and xls files under the selected conversion source folder to csv.")],
+    [sg.Text("The converted csv file will be placed under the converted folder, keeping the folder structure.")],
     [
         sg.InputText(
-            "変換元フォルダを選択",
+            "Select source folder",
             key="-BASE-FOLDER-INPUT-",
             enable_events=True,
         ),
-        sg.FolderBrowse(button_text="選択", font=("メイリオ"), key="-BASE-FOLDER-BROWSE-"),
+        sg.FolderBrowse(button_text="Select", key="-BASE-FOLDER-BROWSE-"),
     ],
     [
         sg.InputText(
-            "変換後フォルダを選択",
+            "Select a folder after conversion",
             key="-CONVERTED-FOLDER-INPUT-",
             enable_events=True,
         ),
         sg.FolderBrowse(
-            button_text="選択", font=("メイリオ"), key="-CONVERTED-FOLDER-BROWSE-"
+            button_text="Select", key="-CONVERTED-FOLDER-BROWSE-"
         ),
     ],
     [sg.Submit()],
 ]
 
 window = sg.Window("ExcelToCSV", layout)
-# Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED:
         break
     if event == "Submit":
-        # csvへの変換を行う
         copy_and_convert_all_excel(
             values["-BASE-FOLDER-INPUT-"], values["-CONVERTED-FOLDER-INPUT-"]
         )
